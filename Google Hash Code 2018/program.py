2834b0e1dbf48af7b9c2df457d427333384e4b05
Greg Brimble <gfb@getadministrate.com>
2018-01-22 00:12:40 +0000

    Remove drawings and add topography

--- Google Hash Code 2018/program.py
+++ Google Hash Code 2018/program.py
@@ -1,7 +1,6 @@
 from collections import Counter
 from itertools import chain
 import logging
-import os
 import sys
 
 class Slice:
@@ -56,71 +55,17 @@ class Slice:
 
 class Cell:
 
-    def __init__(self, j, i, ingredient, top=False, right=False, bottom=False, left=False):
-        self.j = j
-        self.i = i
+    def __init__(self, ingredient):
         self.ingredient = ingredient
-        self.top = top
-        self.right = right
-        self.bottom = bottom
-        self.left = left
-
-    # Applies a slice, creating the visual border on a cell for output
-    def cut(self, slice):
-        if slice.y1 == self.j and slice.x1 <= self.i <= slice.x2:
-            self.top = True
-        if slice.x2 == self.i and slice.y1 <= self.j <= slice.y2:
-            self.right = True
-        if slice.y2 == self.j and slice.x1 <= self.i <= slice.x2:
-            self.bottom = True
-        if slice.x1 == self.i and slice.y1 <= self.j <= slice.y2:
-            self.left = True
-
-    # Draws the cell for output - returns three lines (top, middle and bottom)
-    def draw(self):
-
-        lines = []
-
-        for i in range(3):
-            line = ""
-
-            if self.left:
-                line += "|"
-            else:
-                line += " "
-
-            if i == 0 and self.top:
-                line += "-"
-            elif i == 1:
-                line += self.ingredient
-            elif i == 2 and self.bottom:
-                line += "-"
-            else:
-                line += " "
-
-            if self.right:
-                line += "|"
-            else:
-                line += " "
-
-            lines.append(line)
-
-        return lines
-
-    # Join an array of cell line outputs
-    def join(outputs):
-        lines = []
-        for i in range(3):
-            lines.append([output[i] for output in outputs])
-
-        return lines
 
 
 class Pizza:
 
     matrix = []
-    bins = {}
     slices = []
+    bins = {}
+    hills = []
+    valleys = []
 
     def __init__(self, file_name=None):
         if file_name:
@@ -133,51 +78,19 @@ class Pizza:
         with open(file_name, 'r') as f:
             r, c, self.l, self.h = list(map(int, f.readline().split(" ")))
             for j, line in enumerate(f):
-                self.matrix.append([Cell(j, i, ingredient) for i, ingredient in enumerate(list(line)[:-1])])
+                self.matrix.append([Cell(ingredient) for ingredient in list(line)[:-1]])
 
-        logging.info("Loading complete.")
+        logging.info("Loaded.")
 
-    def output(self, display_matrix=False, display_slices=True, display_bins=True):
-        logging.info("Outputing matrix to display...")
+    def output(self, display_slices=True, display_bins=True, display_hills=True, display_valleys=False):
+        logging.info("Generating output...")
 
         print(" " * 24)
         print("-" * 24)
         print(" " * 24)
 
-        if display_matrix:
-            output_pizza = Pizza()
-            output_pizza.matrix = self.matrix
-
-            if not display_slices:
-                output_pizza.slices = [] # [Slice(0, 0, len(output_pizza.matrix)-1, len(output_pizza.matrix[0])-1)]
-            else:
-                output_pizza.slices = self.slices
-
-            for j, row in enumerate(output_pizza.matrix):
-                outputs = []
-                for i, cell in enumerate(row):
-                    for slice in output_pizza.slices:
-                        cell.cut(slice)
-                    outputs.append(cell.draw())
-
-                lines = Cell.join(outputs)
-
-                for line in lines:
-                    print("".join(line))
-
-            print(" " * 24)
-            print("-" * 24)
-            print(" " * 24)
-
-        if display_slices:
-            for slice in self.slices:
-                print("%i %i %i %i" % (slice.y1, slice.x1, slice.y2, slice.x2))
-
-            print(" " * 24)
-            print("-" * 24)
-            print(" " * 24)
-
         if display_bins:
+            print("Bins")
             for ingredient, count in self.bins.items():
                 print("%s: %i" % (ingredient, count))
 
@@ -185,23 +98,80 @@ class Pizza:
             print("-" * 24)
             print(" " * 24)
 
-        logging.info("Output complete.")
+        if display_hills:
+            print("Hills")
+            for hill in self.hills:
+                print("[%i, %i]" % (hill[0], hill[1]))
+
+            print(" " * 24)
+            print("-" * 24)
+            print(" " * 24)
+
+        if display_valleys:
+            print("Valleys")
+            for valley in self.valleys:
+                print("[%i, %i]" % (valley[0], valley[1]))
+
+            print(" " * 24)
+            print("-" * 24)
+            print(" " * 24)
+
+        if display_slices:
+            print("Slices")
+            for slice in self.slices:
+                print("%i %i %i %i" % (slice.y1, slice.x1, slice.y2, slice.x2))
+
+            print(" " * 24)
+            print("-" * 24)
+            print(" " * 24)
+
+        logging.info("Output generated.")
 
     def bin_count(self):
-        logging.info("Counting bins of matrix...")
+        logging.info("Calculating bins...")
         self.bins = Counter(cell.ingredient for cell in list(chain.from_iterable(self.matrix)))
-        logging.info("Bin counting complete.")
+        logging.info("Bins calculated.")
+
+    def map(self):
+        logging.info("Calculating topography...")
+
+        if not self.bins:
+            self.bin_count()
+
+        hill_ingredient = min(self.bins)
+        valley_ingredient = max(self.bins)
+
+        for j, row in enumerate(self.matrix):
+            for i, cell in enumerate(row):
+                if cell.ingredient == hill_ingredient:
+                    self.hills.append([j, i])
+                elif cell.ingredient == valley_ingredient:
+                    self.valleys.append([j, i])
+
+        logging.info("Topography calculated.")
+
 
     def slice(self, y1, x1, y2, x2):
         self.slices.append(Slice(y1, x1, y2, x2))
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        logging.error("Usage: python program.py [input file]")
+    if len(sys.argv) not in [2, 3]:
+        logging.error("Usage: python program.py [input file] [verbosity=WARNING]")
+
+    if sys.argv[2] == "DEBUG":
+        level = logging.DEBUG
+    elif sys.argv[2] == "INFO":
+        level = logging.INFO
+    elif sys.argv[2] == "WARNING":
+        level = logging.WARNING
+    elif sys.argv[2] == "ERROR":
+        level = logging.ERROR
+    elif sys.argv[2] == "CRITICAL":
+        level = logging.CRITICAL
+
+    logging.basicConfig(level=level)
 
     pizza = Pizza(sys.argv[1])
-    pizza.slice(0, 0, 2, 1)
-    pizza.slice(0, 2, 2, 2)
-    pizza.slice(0, 3, 2, 4)
+    pizza.map()
     pizza.output()
